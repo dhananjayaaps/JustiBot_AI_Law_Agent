502136ecb66885fe331d4adbecda9675fbcf70a6
Thamara <sllakshancc@hotmail.com>
2023-09-03 07:26:33 +0530

    commit

--- main.py
+++ main.py
@@ -1,46 +1,13 @@
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
-from langchain.vectorstores import DocArrayInMemorySearch
-from langchain.document_loaders import TextLoader
-from langchain.chains import RetrievalQA,  ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
-from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import TextLoader
-from langchain.document_loaders import PyPDFLoader
-from langchain.vectorstores import Chroma
-from langchain.llms import OpenAI
+from fastapi import FastAPI
+from pydantic import BaseModel
+from llm import chat
 
-OPENAI_KEY= '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'
+app = FastAPI()
 
-# define embedding
-embeddings = OpenAIEmbeddings(
-    openai_api_key = OPENAI_KEY
-)
-# define memory
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-openai = OpenAI(temperature=0, openai_api_key= OPENAI_KEY)
+class Message(BaseModel):
+    message: str
 
-def create_db(file):
-    # load documents
-    loader = PyPDFLoader(file)
-    documents = loader.load()
-    documents = documents[:1]
-    # split documents
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=30)
-    docs = text_splitter.split_documents(documents)
-    # create vector database from data
-    vectordb = Chroma.from_documents(
-        documents=docs,
-        embedding=embeddings,
-        persist_directory='./chroma_db'
-    )
-    vectordb.persist()
-
-
-
-if __name__ == "__main__":
-    db3 = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
-    qa = ConversationalRetrievalChain.from_llm(openai, db3.as_retriever(), memory=memory, verbose=True)
-    query = "What are the duties of Appointment of Registrar-General"
-    result = qa({"question": query})
-    print(result)
+@app.post("/chat")
+async def send_message(data: Message, response_class=None, response_content_type="text/plain"):
+    res = chat(data.message)
+    return str(res)
